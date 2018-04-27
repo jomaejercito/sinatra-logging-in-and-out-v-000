55c4255e2d89dc65dc7d3e3c8ba152895d0c3860
Joma Ejercito <jomaejercito@gmail.com>
2018-04-27 02:08:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,13 +20,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      @user = User.find_by(id: session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
 end
-
